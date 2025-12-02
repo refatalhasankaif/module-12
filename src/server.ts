62925465923c33a6b262d715485249809b543284
Refat al hasan <refatalhasan911@gmail.com>
2025-12-02 14:44:05 +0600

    vid -6 dopne

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import { Pool } from "pg";
 import dotenv from "dotenv";
 import path from "path";
 
-dotenv.config({path: path.join(process.cwd(), ".env")})
+dotenv.config({ path: path.join(process.cwd(), ".env") })
 
 const app = express()
 const port = 5000;
@@ -15,47 +15,121 @@ const pool = new Pool({
 });
 
 const initDB = async () => {
-  await pool.query(`
-    CREATE TABLE IF NOT EXISTS users(
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    email VARCHAR(150) UNIQUE NOT NULL,
-    age INT,
-    phone VARCHAR(15),
-    address TEXT,
-    created_at TIMESTAMP DEFAULT NOW(),
-    updated_at TIMESTAMP DEFAULT NOW()
-    )
+  try {
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS users(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+        email VARCHAR(150) UNIQUE NOT NULL,
+        age INT,
+        phone VARCHAR(15),
+        address TEXT,
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW()
+      )
     `);
 
     await pool.query(`
       CREATE TABLE IF NOT EXISTS todos(
-      id SERIAL PRIMARY KEY,
-      user_id INT REFERENCES users(id) ON DELETE CASCADE,
-      title VARCHAR(200) NOT NULL,
-      description TEXT,
-      completed BOOLEAN DEFAULT false,
-      due_date DATE,
-      created_at TIMESTAMP DEFAULT NOW(),
-      updated_at TIMESTAMP DEFAULT NOW()
+        id SERIAL PRIMARY KEY,
+        user_id INT REFERENCES users(id) ON DELETE CASCADE,
+        title VARCHAR(200) NOT NULL,
+        description TEXT,
+        completed BOOLEAN DEFAULT false,
+        due_date DATE,
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW()
       )
-      `)
-}
+    `);
+
+    console.log('Tables created successfully!');
+  } catch (error) {
+    console.error('Error creating tables:', error);
+  }
+};
 
 initDB();
 
+// root
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello!')
 });
-app.post('/', (req: Request, res: Response) => {
-  console.log(req.body);
 
-  res.status(201).json({
-    success: true,
-    message: "API is working",
-  })
+// users
+app.post('/users', async (req: Request, res: Response) => {
+  const { name, email } = req.body;
+
+  try {
+    const result = await pool.query(`INSERT INTO  users(name, email) VALUES($1, $2) RETURNING *`, [name, email])
+    // console.log(result.rows[0]);
+    // res.send({message: "data inserted"})
+    res.status(201).json({
+      success: false,
+      message: "Data inserted Successfully",
+      data: result.rows[0],
+    })
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message
+    })
+  }
 
 })
+
+// 
+app.get("/users", async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(`SELECT * FROM users`);
+    res.status(200).json({
+      success: true,
+      message: "Users retrieved successfully",
+      data: result.rows,
+    })
+
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+      details: err
+    })
+  }
+})
+
+app.get("/users/:id", async (req: Request, res: Response) => {
+  // console.log(req.params.id);
+  // res.send({message: "api is cool..........."})
+  try {
+    const result = await pool.query(`SELECT * FROM users WHERE id = $1`, [
+      req.params.id
+    ])
+
+    if(result.rows.length === 0){
+      res.status(404).json({
+              success: false,
+      message: "User not found",
+      })
+    }else{
+      res.status(200).json({
+        success: true,
+        message: "User fetched successfully",
+        data: result.rows[0],
+      })
+    }
+
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    })
+  }
+})
+
+
+
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
